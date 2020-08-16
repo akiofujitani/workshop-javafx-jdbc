5b4a9adbf9e5b74635a727c392bcb4f81944beca
Fausto Akio <akiofujitani@yahoo.co.jp>
2020-08-16 19:33:12 -0300

    Saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,18 +3,27 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
 
-	//Department entity to make possible to inject dependence
+	//Department entity to make dependence injection
 	private Department entity;
+	
+	//DepartmentService service to make dependence injection
+	private DepartmentService service;
 	
 	@FXML
 	private TextField txtId;
@@ -32,13 +41,26 @@ public class DepartmentFormController implements Initializable {
 	private Button btCancel;
 		
 	@FXML
-	public void onBtSaveAction() {
-		System.out.println("onBtSaveAction");
+	public void onBtSaveAction(ActionEvent event) {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			Utils.currentStage(event).close();
+		}
+		catch (DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
 	}
-	
+
 	@FXML
-	public void onBtCancelAction() {
-		System.out.println("onBtCancelAction");
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
  	
 	@Override
@@ -49,6 +71,11 @@ public class DepartmentFormController implements Initializable {
 	//method for dependence injection passing an Department entity to this entity
 	public void setDepartment(Department entity) {
 		this.entity = entity;
+	}
+	
+	//method for dependence injection passing an DepartmentService service to this service
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 	
 	//Set limit values to text fields
@@ -64,5 +91,16 @@ public class DepartmentFormController implements Initializable {
 		}
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
+	}
+	
+	
+	//Get values from text field and insert it into Department object
+	private Department getFormData() {
+		Department obj = new Department();
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		
+		return obj;
 	}
 }
